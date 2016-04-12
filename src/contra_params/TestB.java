2f2b737a2189d6536aabd451d244a4053d58feb9
Alexandre Terrasa <alexandre@moz-code.org>
2016-04-12 12:07:35 -0400

    add examples on parameters type variance
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

---
+++ src/contra_params/TestB.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2015 Alexandre Terrasa <alexandre@moz-code.org>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package contra_params;
+
+import test.A;
+
+public class TestB extends TestA {
+
+    // Try to redefine foo with contravariant parameter type
+    public void foo(A b) {
+        System.out.println("TestB::foo(A)");
+    }
+}
